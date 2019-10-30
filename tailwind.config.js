1fb10e6b8d81ccf2512169f71b6b46c0784db3a3
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-10-30 10:22:51 +0100

    Tailwind CSS config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                brand: '#006738',
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+};
