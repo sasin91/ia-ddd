05900ec312db6efda79822cb0a320d719b0d6385
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2019-10-31 20:03:07 +0100

    Added test for login with email

--- tests/Feature/LoginTest.php
+++ tests/Feature/LoginTest.php
@@ -4,8 +4,8 @@ namespace Tests\Feature;
 
 use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 use function factory;
 
@@ -13,13 +13,58 @@ class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Encrypting the same password over and over is expensive, so we store and reuse it.
+     *
+     * @decryptedValue password
+     * @var string
+     */
+    protected $passwordHash = '';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->passwordHash = Hash::make('password');
+    }
+
+    public function test_can_login_with_username_instead_of_email()
+    {
+        $user = factory(User::class)->create([
+            'username' => 'john_doe',
+            'password' => $this->passwordHash
+        ]);
+
+        $this->post('/login', [
+            'username' => 'john_doe',
+            'password' => 'password'
+        ])->assertRedirect('/home');
+
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_can_login_with_email()
+    {
+        $user = factory(User::class)->create([
+            'email' => 'john@example.com',
+            'password' => $this->passwordHash
+        ]);
+
+        $this->post('/login', [
+            'email' => 'john@example.com',
+            'password' => 'password'
+        ])->assertRedirect('/home');
+
+        $this->assertAuthenticatedAs($user);
+    }
+
     public function test_it_stores_a_login()
     {
         Date::setTestNow('2019-10-29 23:43:00');
 
         $user = factory(User::class)->create([
             'email' => 'john@example.com',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => $this->passwordHash
         ]);
 
         $this->post('/login', [
@@ -33,20 +78,5 @@ class LoginTest extends TestCase
             'user_agent' => 'Symfony',
             'created_at' => '2019-10-29 23:43:00'
         ]);
-    }
-
-    public function test_can_login_with_username_instead_of_email()
-    {
-        $user = factory(User::class)->create([
-            'username' => 'john_doe',
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        ]);
-
-        $this->post('/login', [
-            'username' => 'john_doe',
-            'password' => 'password'
-        ])->assertRedirect('/home');
-
-        $this->assertAuthenticatedAs($user);
     }
 }
